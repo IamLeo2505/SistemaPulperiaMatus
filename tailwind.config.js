e0013dca9e2a3bddd3c4f69b4449a89e9a72a8fb
unknown101101k <118642472+unknown101101k@users.noreply.github.com>
2025-04-15 19:43:10 -0600

    Feat(frontend): Agregar Tailwind CSS para el estilado y Alpine JS para interactividad.
    -Se instalaron las dependencias de Tailwind CSS, PostCSS y Autoprefixer.
    -Se inicializó la configuración de Tailwind y se importaron las directivas en app.css.
    -Se instaló el paquete alpinejs vía npm y se importó en app.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
  /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
+   content: [
+    "./src/**/*.{html,js}",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./vendor/livewire/livewire/**/*.blade.php",
+  ],
    theme: {
      extend: {},
    },
