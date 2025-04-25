1423cf2ffd468fd1d7c295fd96521f1ae9fbd615
unknown101101k <118642472+unknown101101k@users.noreply.github.com>
2025-04-25 14:04:16 -0600

    Feat(migrations): Corrección de llaves foraneas en las migraciones, eliminación de tabla inventario.
    Corrección de modelos pendiente.

--- database/migrations/2025_04_06_042956_create_productos_table.php
+++ database/migrations/2025_04_06_042956_create_productos_table.php
@@ -14,12 +14,15 @@ return new class extends Migration
     Schema::create('productos', function (Blueprint $table) {
         $table->id();
         $table->string('image_path', 100);
+        $table->string('nombre', 100);
         $table->string('descripcion', 255);
         $table->string('codigo_barras', 45);
         $table->string('marca', 45);
+        $table->integer('cantidadstock');
+        $table->integer('stockminimo');
+        $table->date('fechavencimiento');
         $table->foreignId('unidad_medida_id')->constrained('unidad_medida');
         $table->foreignId('categoria_id')->constrained('categoria');
-        $table->foreignId('proveedor_id')->constrained('proveedor');
         $table->timestamps();
     });
 }
--- database/migrations/2025_04_06_045506_create_precio_producto_table.php
+++ database/migrations/2025_04_06_045506_create_precio_producto_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
     {
         Schema::create('precio_producto', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('producto_id')->constrained('productos')->onDelete('cascade');
             $table->decimal('preciocompra', 10, 2);
             $table->decimal('precioventa', 10, 2);
             $table->decimal('impuesto_iva', 10, 2);
-            $table->foreignId('unidad_medida_id')->constrained('unidad_medida');
-            $table->foreignId('categoria_id')->constrained('categoria');
-            $table->foreignId('inventario_id')->constrained('inventario');
-            $table->foreignId('proveedor_id')->constrained('proveedor');
+            $table->foreignId('producto_id')->constrained('productos')->onDelete('cascade');
             $table->timestamps();
         });
     }
