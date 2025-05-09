61b1e78611b1e9c9346081ad74b8540cac884f7f
unknown101101k <118642472+unknown101101k@users.noreply.github.com>
2025-05-08 18:53:18 -0600

    Fix(system): este es el commit definitivo.

--- app/Models/usuario.php
+++ app/Models/usuario.php
@@ -9,24 +9,19 @@ class Usuario extends Authenticatable
 {
     use HasFactory;
 
-<<<<<<<< HEAD:app/Models/Usuario.php
     protected $table = 'usuarios';
-    protected $fillable = ['user', 'password', 'empleado_id'];
+
+    protected $fillable = ['user', 'password', 'empleado_id', 'image_path_Usuarios'];
+
     protected $hidden = ['password'];
 
-    
     public function getAuthIdentifierName()
     {
         return 'user';
     }
-========
-    protected $fillable = ['image_path_Usuarios', 'user', 'password', 'empleado_id'];
->>>>>>>> 410f8788e06a8ad6a77ce8901c9e821d053d6f56:app/Models/usuario.php
 
     public function empleado()
     {
         return $this->belongsTo(Empleado::class, 'empleado_id');
     }
 }
-
-
